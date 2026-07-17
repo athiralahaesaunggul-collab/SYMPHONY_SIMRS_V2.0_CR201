714b1232f259bf3075975fc99d3ffe015a5dd4b8
athira <athiralahaesaunggul.com>
2026-07-17 14:21:13 +0700

    fix: ubah base vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,14 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/symphony_simrs_v2.0_cr201/',
+  base: '/',
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000,
     host: '0.0.0.0', // Bisa diakses dari IP jaringan lokal
     watch: {
       // Baris ini wajib ditambahin biar Vite gak ngawasin folder server!
-      ignored: ['**/server/**'] 
+      ignored: ['**/server/**']
     },
     proxy: {
       // Semua request ke /api akan di-forward ke backend Express di port 5000
